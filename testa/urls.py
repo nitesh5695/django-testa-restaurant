0dc3d9b4e7638c4ba5f8198194904e06c6800386
Nitesh <nitesh.singh5695@gmail.com>
2020-12-25 15:24:52 +0530

    django-restaurant-project2

--- testa/urls.py
+++ testa/urls.py
@@ -16,8 +16,16 @@ MAIN URLS
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.views.static import server
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('customer.urls'))
+    path('',include('customer.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}) 
 ]
